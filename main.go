76336825bb0eacd3e21d2f8ebbb121baed93244b
e10ulen <mr.potetohead@yahoo.ne.jp>
2017-12-14 01:59:25 +0900

    Commit :2017/12/14 01:59

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"fmt"
 	"time"
+	"bufio"
 )
 const df = "2006/01/02 15:04"
 func main(){
@@ -13,5 +14,16 @@ func main(){
 		fmt.Print(dir, "\n")
 	}
 	fmt.Print("DateTime:", tm.Format(df), "\n")
-
+//	Scan Stdin
+	scr := bufio.NewScanner(os.Stdin)
+	fmt.Print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nScan Text > ")
+	for scr.Scan(){
+			fmt.Println(scr.Text())
+			if scr.Text() == string("exit") {
+				os.Exit(0)
+			}
+	}
+	if err := scr.Err();err != nil {
+		fmt.Fprintln(os.Stderr, "readinf standerd input", err)
+	}
 }
