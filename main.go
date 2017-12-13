4443c2df79b6cec2924e8cb53ed2b4310455ea1f
e10ulen <mr.potetohead@yahoo.ne.jp>
2017-12-13 23:13:23 +0900

    Commit :2017/12/13 23:13

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"log"
+	"fmt"
+	"time"
+)
+const df = "2006/01/02 15:04"
+func main(){
+	tm := time.Now()
+	dir, err := os.Getwd()
+	if err == nil {
+		fmt.Print(dir, "\n")
+	}
+	fmt.Print("DateTime:", tm.Format(df), "\n")
+	boot, err := exec.Command("clear").CombinedOutput()
+	if err != nil {
+		log.Print("Error NotClearScreen\n",boot)
+	}
+}
