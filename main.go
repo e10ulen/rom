24e6f9f68b4226594f4059304b3396d1ba3f694c
e10ulen <mr.potetohead@yahoo.ne.jp>
2017-12-16 17:12:53 +0900

    Commit :2017/12/16 17:12

--- main.go
+++ main.go
@@ -15,18 +15,41 @@ func main() {
 	}
 	dn := "src"
 	fp := "src/hello"
-	readDir(dn)
-	makeDir(dn)
-	readDir(dn)
-	readFile()
-
-	touchFile()
-	createFile(fp)
-	readFile()
+	loadFile(dn,fp)
+	// readDir(dn)
+	// makeDir(dn)
+	// readDir(dn)
+	// readFile()
+  //
+	// touchFile()
+	// createFile(fp)
+	// readFile()
 }
 // htmlソースを読み込んで、htmlの中に規定の記述があればロードしてある別のファイルを挿入する
-func loadFile()  {
+func loadFile(dn string,fp string)  {
+	//	readDir部分
+	files, err := ioutil.ReadDir(dn)
+	if err != nil {
+		fmt.Print("Not Load Dir")
+	}
+	for _, file := range files {
+		fmt.Println("ReadDir:",file.Name())
+	}
+	//	createFile部分
+	file, err := os.OpenFile(fp, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Print(err)
+	}
+	defer file.Close()
 
+	fmt.Fprintln(file, "Hello")
+
+	//	readFile部分
+	content, err := ioutil.ReadFile("src/hello")
+	if err != nil {
+		log.Print(err)
+	}
+	fmt.Print("File contents: ", string(content), "\n")
 }
 func createFile(fp string)  {
 	file, err := os.OpenFile(fp, os.O_APPEND|os.O_WRONLY, 0600)
